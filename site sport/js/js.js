82c458e158bd7d8debc9a95dc3c660bc8bc31ced
youssef khemakhem <99668071+youssef195@users.noreply.github.com>
2022-10-24 13:47:29 +0200

    Add files via upload

---
+++ site sport/js/js.js
@@ -0,0 +1,10 @@
+const navslide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navslide();
